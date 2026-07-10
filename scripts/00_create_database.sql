b0916a37246ff29c4e4ddf024a68589399c32cf2
ykartik012007-sudo <ykartik012007@gmail.com>
2026-07-10 09:20:54 +0530

    Create initial database and schemas for e-commerce

---
+++ scripts/00_create_database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE brazilian_e_commerce_data_warehouse;
+GO
+
+USE brazilian_e_commerce_data_warehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
