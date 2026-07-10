c0209fcd8af426fd090dcce02376fb27ce4fa2db
ykartik012007-sudo <ykartik012007@gmail.com>
2026-07-10 09:25:17 +0530

    Enhance script with comments for clarity
    Added comments to describe the purpose of the script and the database structure.

--- scripts/00_create_database.sql
+++ scripts/00_create_database.sql
@@ -1,3 +1,10 @@
+/*
+    Script  : This script creates the database 'brazilian_e_commerce_data_warehouse'
+              and the Bronze, Silver, and Gold schemas.
+
+    Purpose : Store source data in the database and prepare it for business analysis.
+*/
+
 CREATE DATABASE brazilian_e_commerce_data_warehouse;
 GO
 
